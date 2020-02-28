416e9f26ed27179399e04463adf9f3a8ce8b1fd4
Pulkit Saxena <saxenap@aetna.com>
2020-02-28 14:19:37 -0500

    Assignment-01 Solution

---
+++ basics-assignment-1/assignment.js
@@ -0,0 +1,10 @@
+const userInput = 20;
+let result;
+
+result = userInput + 18;
+result = result - 2;
+result = result * 2;
+result = result / 36;
+
+alert(`result:: ${result}`);
+alert(`userInput:: ${userInput}`);
